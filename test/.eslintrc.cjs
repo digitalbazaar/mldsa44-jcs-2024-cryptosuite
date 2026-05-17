fc9fef57fb36a3c97f05026dde7a27fa48a66274
Manu Sporny <msporny@digitalbazaar.com>
2026-05-17 15:34:52 -0400

    Add boilerplate repo config files.

---
+++ test/.eslintrc.cjs
@@ -0,0 +1,13 @@
+/*!
+ * Copyright (c) 2023-2026 Digital Bazaar, Inc.
+ */
+module.exports = {
+  globals: {
+    should: true,
+    assertNoError: true
+  },
+  env: {
+    node: true,
+    mocha: true
+  }
+};
